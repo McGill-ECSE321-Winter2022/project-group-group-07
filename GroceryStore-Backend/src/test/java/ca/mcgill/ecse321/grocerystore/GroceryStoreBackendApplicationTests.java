deac7d3d32556e1327504b0f00654d96a11c7681
KarimSabriMoussa <karim.moussa@mail.mcgill.ca>
2022-02-22 17:29:27 -0500

    changed package name

---
+++ GroceryStore-Backend/src/test/java/ca/mcgill/ecse321/grocerystore/GroceryStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.grocerystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
