896018acf8c9cc960eba33cd367dbf0f07c9efb7
KarimSabriMoussa <karim.moussa@mail.mcgill.ca>
2022-02-22 20:32:41 -0500

    misc

--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/grocerystore/dao/AccountRepository.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/grocerystore/dao/AccountRepository.java
@@ -2,7 +2,13 @@ package ca.mcgill.ecse321.grocerystore.dao;
 
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.grocerystore.model.Account;
+import ca.mcgill.ecse321.grocerystore.model.AccountRole;
 
 public interface AccountRepository extends CrudRepository<Account,String> {
 	
+	Account findByUsername(String username);
+	
+	Account findByName(String name);
+	
+	Account findByAccountRole(AccountRole accountRole);
 }
