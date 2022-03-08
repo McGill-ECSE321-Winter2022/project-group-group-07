2f31231718de0e076576e77c0ed9ccce2be74808
KarimSabriMoussa <karim.moussa@mail.mcgill.ca>
2022-03-07 22:34:52 -0500

    initial setup for Sprint 2

---
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/grocerystore/controller/GroceryStoreRestController.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse321.grocerystore.controller;
+
+public class GroceryStoreRestController {
+
+}
---
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/grocerystore/service/GroceryStoreService.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse321.grocerystore.service;
+
+public class GroceryStoreService {
+
+}
