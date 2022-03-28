3fb9bfb5cbe8db979ea847aabc73456747cc6e4d
Jasmine Cheung <jasmine.cheung@mail.mcgill.ca>
2022-03-28 00:57:34 -0400

    Customer Account informations page

--- grocerystore-frontend/src/router/index.js
+++ grocerystore-frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import AccountInfo from '@/components/AccountInfo'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/AccountInfo',
+      name: 'AccountInfo',
+      component: AccountInfo
     }
   ]
 })
