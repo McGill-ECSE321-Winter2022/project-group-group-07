cd3d71ff4c06ed2e06d4b294c9a7aa336336112a
KarimSabriMoussa <karim.moussa@mail.mcgill.ca>
2022-04-02 15:50:24 -0400

    frontend - Owner pages

--- grocerystore-frontend/src/router/index.js
+++ grocerystore-frontend/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Hello from '@/views/Hello'
+import StoreInfo from '@/views/StoreInfo'
+import ManageEmployees from '@/views/ManageEmployees'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/s',
+      name: 'StoreInfo',
+      component: StoreInfo
+    },
+    {
+      path: '/a',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/',
+      name: 'ManageEmployees',
+      component: ManageEmployees
     }
   ]
 })
