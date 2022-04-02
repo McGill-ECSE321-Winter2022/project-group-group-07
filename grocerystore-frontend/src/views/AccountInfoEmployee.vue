8e05753c66c82267929ca0ec96ba846b3f0b0be9
eliasaboufarah02 <72624529+eliasaboufarah02@users.noreply.github.com>
2022-04-02 18:47:14 -0400

    complete front end without link to backend

--- grocerystore-frontend/src/views/AccountInfoEmployee.vue
+++ grocerystore-frontend/src/views/AccountInfoEmployee.vue
@@ -18,21 +18,63 @@
         <label id = "username" > {{customerUsername}} </label>
         <br>
         <label>Name: </label>
-        <label id = "name" > {{customerName}} </label><br>
+        <label id = "name" > {{customerName}} </label>
         <button @click="changePassword()" class="block magenta margin-auto"> Change Password </button>
         
         <span v-if="errorName" style="color:red">Error: {{errorName}} </span>
         <br>
         
         <label>Address: </label>
-        <label id = "address" > {{customerAddress}} </label><br>
+        <label id = "address" > {{customerAddress}} </label>
         <button @click="changeAddress()"> Change Address </button><br>
 
         <br>
 
     </div>
     <br>
-
+    <label style="font-size: 24px">Schedule</label>
+      <table>
+        <tr>
+          <th>Day</th>
+          <th>Start Time</th>
+          <th>End Time</th>
+        </tr>
+        <tr>
+          <td>Monday</td>
+          <td>{{ MondayST }}</td>
+          <td>{{ MondayET }}</td>
+        </tr>
+        <tr>
+          <td>Tuesday</td>
+          <td>{{ TuesdayST }}</td>
+          <td>{{ TuesdayET }}</td>
+        </tr>
+        <tr>
+          <td>Wednesday</td>
+          <td>{{ WednesdayST }}</td>
+          <td>{{ WednesdayET }}</td>
+        </tr>
+        <tr>
+          <td>Thursday</td>
+          <td>{{ ThursdayST }}</td>
+          <td>{{ ThursdayET }}</td>
+        </tr>
+        <tr>
+          <td>Friday</td>
+          <td>{{ FridayST }}</td>
+          <td>{{ FridayET }}</td>
+        </tr>
+        <tr>
+          <td>Saturday</td>
+          <td>{{ SaturdayST }}</td>
+          <td>{{ SaturdayET }}</td>
+        </tr>
+        <tr>
+          <td>Sunday</td>
+          <td>{{ SundayST }}</td>
+          <td>{{ SundayET }}</td>
+        </tr>
+      </table>
 </div>
 </template>
 <script>
@@ -87,5 +129,17 @@
     border-style:solid;
     border-color: azure;
     border-radius: 0.5em;
+}
+
+table {
+  margin-left: 2em;
+  width: 90%;
+  border-collapse: collapse;
+}
+th,
+td {
+  border: 1px solid black;
+  padding: 5px;
+  text-align: left;
 }
 </style>
