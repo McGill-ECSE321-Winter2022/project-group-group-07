06d52f6418a2d3f15208754021150c350100fff9
Jasmine Cheung <jasmine.cheung@mail.mcgill.ca>
2022-04-15 03:17:03 -0400

    Modif

--- grocerystore-frontend/src/views/Login.vue
+++ grocerystore-frontend/src/views/Login.vue
@@ -25,6 +25,11 @@
           color="black"
           style="margin-top:10px; width:203px;"
         />
+          <Button @btn-click="routeToSignup()"
+          text="Signup"
+          color="black"
+          style="margin-top:10px; width:203px;"
+        />
       </form>
     </div>
   </div>
@@ -68,6 +73,7 @@ export default {
     login: function(){
         AXIOS.post('/api/account/login/?'+"username="+this.username+"&password="+this.password)
         .then(response => {
+          console.log(response.data);
           localStorage.setItem('token',response.data.username);
           localStorage.setItem('pointBalance',response.data.pointBalance);
           localStorage.setItem('role',response.data.role);
