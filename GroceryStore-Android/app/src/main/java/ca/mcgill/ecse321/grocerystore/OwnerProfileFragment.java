5ceb5b5cbe1d906fbadc98c76171d7ec397cf35a
Jasmine Cheung <jasmine.cheung@mail.mcgill.ca>
2022-04-17 18:21:45 -0400

    Profile Redirection from Login is working

--- GroceryStore-Android/app/src/main/java/ca/mcgill/ecse321/grocerystore/OwnerProfileFragment.java
+++ GroceryStore-Android/app/src/main/java/ca/mcgill/ecse321/grocerystore/OwnerProfileFragment.java
@@ -1,6 +1,41 @@
 package ca.mcgill.ecse321.grocerystore;
 
-import android.app.Fragment;
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.fragment.NavHostFragment;
 
 public class OwnerProfileFragment extends Fragment {
+
+    private View ownerProfileView;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        ownerProfileView = inflater.inflate(R.layout.owner_profile, container, false);
+        return ownerProfileView;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ownerProfileView.findViewById(R.id.OwnerViewAccountInfo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(OwnerProfileFragment.this)
+                        .navigate(R.id.action_OwnerProfileFragment_to_NonCustomerInfoFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ownerProfileView = null;
+    }
 }
