f812bfea795e8feb5b70047f1f256998ea8cfe6c
KarimSabriMoussa <karim.moussa@mail.mcgill.ca>
2022-04-17 18:27:01 -0400

    catalogue adnroid frontend setup

--- GroceryStore-Android/app/src/main/java/ca/mcgill/ecse321/grocerystore/CatalogueFragment.java
+++ GroceryStore-Android/app/src/main/java/ca/mcgill/ecse321/grocerystore/CatalogueFragment.java
@@ -1,4 +1,36 @@
 package ca.mcgill.ecse321.grocerystore;
 
-public class CatalogueFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class CatalogueFragment extends Fragment {
+
+    private View catologueView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        catologueView = inflater.inflate(R.layout.fragment_catalogue,container,false);
+        return catologueView;
+    }
+
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
+
+        super.onViewCreated(view,savedInstanceState);
+        catologueView.findViewById(R.id.addToCartButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+
+        });
+    }
+
+
 }
